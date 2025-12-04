1a7ff13254474346812b8b89057e059527ad09cb
Rayya Syauqi Alulu'i <152264203+Rayya12@users.noreply.github.com>
2025-12-04 10:35:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.get("/")
 def home():
-    return "Halo, Kami dari kelompok 1 ini aplikasi kami"
+    return {"message": "Hello from Flask on Railway!"}
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8000)
